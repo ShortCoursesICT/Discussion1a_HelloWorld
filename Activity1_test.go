7d8581de83d1687e4b6bad4b56f624a22a25b174
ShortCoursesICT <ict.cet.shortcourses@gmail.com>
2022-03-19 10:59:11 +0800

    Changes to testing without G

--- Activity1_test.go
+++ Activity1_test.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"testing"
-
-	. "github.com/franela/goblin"
 )
 
 func TestHelloWorld(t *testing.T) {
-	g := Goblin(t)
-	g.Describe("Activity 1", func() {
-		g.Assert(HelloWorld()).Equal("Hello World from Go !!")
-	})
+	got := HelloWorld()
+	res := "Hello World from Go !!"
+	if got != "Hello World from Go !!" {
+		t.Errorf("HelloWorld() = %s; want %s", got, res)
+	}
 
 }

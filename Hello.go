3475e0fc2b0136fbfec9a801fa2f9635ada80346
ShortCoursesICT <ict.cet.shortcourses@gmail.com>
2022-03-20 19:28:38 +0800

    final test

--- Hello.go
+++ Hello.go
@@ -2,7 +2,7 @@ package main
 
 func HelloWorld() string {
 	//Insert your code here
-	messageHelloWorld := "Hello World !!"
+	messageHelloWorld := "Hello World from Go !!"
 	//Do not remove this line here
 	return messageHelloWorld
 }
